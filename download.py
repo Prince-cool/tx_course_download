4f19a3632927bb4f5c3099b0f710cf64e507cf81
prince <62496012+Prince-cool@users.noreply.github.com>
2022-05-05 16:10:30 +0800

    Update download.py

--- download.py
+++ download.py
@@ -63,7 +63,7 @@ while True:
     'authority': 'ke.qq.com',
     'accept': '*/*',
     'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-    'cookie': 'pgv_pvid=4331793015; eas_sid=H1h6q4U7H5q9M652q8G6S4D2F9; RK=btVx/bHBee; ptcz=9bfa9d14b2107392c520e16cdbc92bfdfbcd5a07022db22b1c33d784ad4b74ee; ts_refer=cn.bing.com/; iswebp=1; auth_version=2.0; mix_login_mode=true; ptui_loginuin=1967416560; luin=o1967416560; lskey=000100006f4006ca0f64a90bd2dda240b22c4da0704832ac8ef76b2fc679035c9f04b85324d080c96fe1d1c6; p_lskey=000400009cbdc1b24ee9171008e57653d734afbebcd6d6d7b098597937a8930d124c2ac0651bec6e953da1f5; uid_type=0; uin=1967416560; p_uin=1967416560; p_luin=1967416560; uid_uin=1967416560; uid_origin_uid_type=0; uid_origin_auth_type=0; ke_login_type=1; sessionPath=16516774470057034474434; _pathcode=0.17126931133406575; tdw_auin_data=-; tdw_data_testid=; tdw_data_flowid=; tdw_first_visited=1; pgv_info=ssid=s9771023168; ts_last=ke.qq.com/; ts_uid=9571296640; Hm_lvt_0c196c536f609d373a16d246a117fd44=1651643810,1651654054,1651677448; Hm_lpvt_0c196c536f609d373a16d246a117fd44=1651677448; tdw_data={"ver4":"cn.bing.com","ver5":"","ver6":"","refer":"cn.bing.com","from_channel":"","path":"aB-0.17126931133406575","auin":"-","uin":"1967416560","real_uin":"967416560"}; tdw_data_new_2={"auin":"-","sourcetype":"","sourcefrom":"","ver9":"1967416560","uin":"1967416560","visitor_id":"7631666100227017","ver10":"","url_page":"","url_module":"","url_position":"","sessionPath":"16516774470057034474434"}',
+    'cookie': cookie,
     'referer': 'https://ke.qq.com/user/index/index.html',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36 Edg/101.0.1210.32'
   }
